c1a2699c40024ca78250b7a5bcaa41ab729165bb
sunluomeng <lmsun>
2020-04-15 16:15:09 +0800

    添加生成配置工具类,完善环境配置

---
+++ springboot-mybatis-demo/src/main/java/com/lqcoder/mybatisdemo/generate/StartUp.java
@@ -0,0 +1,35 @@
+package com.lqcoder.mybatisdemo.generate;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: luomengsun
+ * @mail killerslm@163.com
+ * @create: 2020-04-15 16:13
+ * @description:
+ **/
+public class StartUp {
+    public static void main(String[] args){
+        try {
+            List<String> warnings = new ArrayList<String>();
+            boolean overwrite = true;
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            InputStream is = classloader.getResourceAsStream("generatorConfig.xml");
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config = cp.parseConfiguration(is);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
